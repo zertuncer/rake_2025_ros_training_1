298010bc0d4e3eb078941a271f407ca594ceab22
zertuncer <zertuncer@gmail.com>
2025-02-08 19:04:21 +0300

    second commid

---
+++ build/rake_projesi/robot_description/CMakeFiles/robot_description_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/mnt/c/Users/ros/catkin_ws/devel/include/robot_description/WoundedInfo.h"
+  "/mnt/c/Users/ros/catkin_ws/devel/include/robot_description/WoundedInfoArray.h"
+  "CMakeFiles/robot_description_generate_messages_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/robot_description_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
