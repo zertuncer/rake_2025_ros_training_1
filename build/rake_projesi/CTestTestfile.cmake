7235c4f1f14c8b327904218ca842775a9c8f9869
zertuncer <zertuncer@gmail.com>
2025-02-08 19:03:28 +0300

    second commid

--- build/rake_projesi/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/ros/catkin_ws/src/rake_projesi
-# Build directory: /mnt/c/Users/ros/catkin_ws/build/rake_projesi
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
